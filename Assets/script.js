5464c70576a419a442b8d5abb0cb6cae0c85f308
mkdemon5 <alvaradomike5@gmail.com>
2022-12-06 00:18:14 -0600

    pushing changes

--- Assets/script.js
+++ Assets/script.js
@@ -21,10 +21,16 @@ var formBtn = document.createElement('button');
 formBtn.setAttribute('id', 'searchBtn');
 formBtn.textContent = 'Search';
 
+// Creating a button to clear search history
+var clearBtn = document.createElement('button');
+clearBtn.setAttribute('id', 'clearBtn');
+clearBtn.textContent = 'Clear History';
+
 // Appending the pieces of the form to the form
 newForm.append(formLabel);
 newForm.append(formInput);
 newForm.append(formBtn);
+newForm.append(clearBtn);
 
 // Appending the form to the Side Bar
 document.querySelector('.searchBar').appendChild(newForm);
@@ -34,10 +40,17 @@ init();
 // Adding event listeners to buttons from past searches
 for (var i = 0; i < searchHist.length; i++) {
 	var tempBtn = document.getElementById(searchHist[i]);
-	tempBtn.addEventListener('click', repeatSearch(searchHist[i]));
+	tempBtn.addEventListener('click', cityLocation(searchHist[i]));
 }
 
-// Adding event listener to the button
+// Adding event listener to the clear history button
+let clrHistBtn = document.getElementById('clearBtn');
+clrHistBtn.addEventListener('click', function (event) {
+	event.preventDefault();
+	searchHist.length = 0;
+});
+
+// Adding event listener to the search button
 let searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', function (event) {
 	event.preventDefault();
@@ -47,7 +60,7 @@ searchBtn.addEventListener('click', function (event) {
 	// Logging the input to local storage
 	var value = path[1][0].value;
 	if (searchHist.includes(value)) {
-		repeatSearch(value);
+		cityLocation(value);
 	} else {
 		searchHist.push(value.trim());
 		for (var i = 0; i < searchHist.length; i++) {
@@ -60,18 +73,18 @@ searchBtn.addEventListener('click', function (event) {
 
 // Function to pull content from local storage from previous visit(s) on page load
 function init() {
-	var i = 0;
-	searchHist.length = 0;
-	while (localStorage.getItem('City' + i)) {
-		searchHist.push(localStorage.getItem('City' + i));
-		console.log(searchHist[i]);
-		i++;
-	}
 	pastSearch();
 }
 
-// Function to create buttons for previous searches
+// Function to pull content from local storage from previous visit(s) on page load & create buttons for previous searches
 function pastSearch() {
+	var i = 0;
+	searchHist.length = 0;
+	while (localStorage.getItem(`City${i}`)) {
+		searchHist.push(localStorage.getItem(`City${i}`));
+		console.log(searchHist[i]);
+		i++;
+	}
 	for (var i = 0; i < searchHist.length; i++) {
 		createButton(searchHist[i]);
 	}
@@ -80,19 +93,14 @@ function pastSearch() {
 // Function to create button after search or on init of page
 function createButton(city) {
 	var newBtn = document.createElement('button');
-	newBtn.classList.add('pastBtn');
+	newBtn.classList.add('btn', 'btn-block');
 	newBtn.setAttribute('id', city);
 	newBtn.textContent = city;
 
 	var newLiEl = document.createElement('li');
-	newLiEl.classList.add('list-group');
+	newLiEl.classList.add('list-group', 'history-button');
 	newLiEl.append(newBtn);
-	document.querySelector('.pastBar').appendChild(newLiEl);
-}
-
-// Function to pull API data for a previous search result
-function repeatSearch(city) {
-	cityLocation(city);
+	document.querySelector('.searchBar').appendChild(newLiEl);
 }
 
 // Function to gather latitude and longitude coordinates of city for weather API call
@@ -126,24 +134,54 @@ function getWeather(lat, lon) {
 			return response.json();
 		})
 		.then((data) => {
-			let description = data.weather[0].description;
-			console.log(description);
-			let weatherId = data.weather[0].main;
-			console.log(weatherId);
+			let name = data.name;
+			console.log(name);
+			let icon = data.weather[0].icon;
+			console.log(icon);
+			let weatherDescription = data.weather[0].main;
+			console.log(weatherDescription);
 			let windSpeed = data.wind.speed;
 			console.log('Wind speed: ' + windSpeed + ' mph');
-			let clouds = data.clouds.all;
-			console.log('Cloudiness %: ' + clouds);
 			let currentTemp = Math.round(data.main.temp);
 			console.log(currentTemp + '\u00B0');
 			let humidity = data.main.humidity;
 			console.log('Humidity %: ' + humidity);
 			let sunUp = new Date(data.sys.sunrise * 1000);
-			let sunRise = sunUp.getHours() + ':' + sunUp.getMinutes();
+			let sunRise, sunSet;
+			if (sunUp.getMinutes() < 10) {
+				sunRise = sunUp.getHours() + ':0' + sunUp.getMinutes();
+			} else {
+				sunRise = sunUp.getHours() + ':' + sunUp.getMinutes();
+			}
 			console.log('Sunrise: ' + sunRise + 'am');
 			let sunDown = new Date(data.sys.sunset * 1000);
-			let sunSet = sunDown.getHours() - 12 + ':' + sunDown.getMinutes();
+			if (sunDown.getHours() < 12) {
+				sunSet = sunDown.getHours() + ':' + sunDown.getMinutes();
+			} else {
+				sunSet = sunDown.getHours() - 12 + ':' + sunDown.getMinutes();
+			}
 			console.log('Sunset: ' + sunSet + 'pm');
+			let today =
+				sunDown.getMonth() +
+				1 +
+				'/' +
+				sunUp.getDate() +
+				'/' +
+				sunUp.getFullYear();
+			console.log(today);
+			console.log(data);
+
+			mainCard(
+				name,
+				icon,
+				weatherDescription,
+				windSpeed,
+				currentTemp,
+				humidity,
+				sunRise,
+				sunSet,
+				today
+			);
 		});
 }
 
@@ -160,9 +198,90 @@ function getForecast(lat, lon) {
 		})
 		.then((data) => {
 			console.log(data);
+			forecastCards(data);
 		});
 }
 
-function mainCard() {}
+function mainCard(
+	city,
+	weatherIcon,
+	description,
+	windSpeed,
+	currentTemp,
+	humidity,
+	sunRise,
+	sunSet,
+	date
+) {
+	var icon = document.getElementById(`img`);
+	icon.setAttribute(
+		'src',
+		`http://openweathermap.org/img/wn/${weatherIcon}.png`
+	);
+	icon.setAttribute('alt', description);
 
-function forecastCards() {}
+	var cardHeader = document.getElementById('card-header');
+	cardHeader.textContent = `${city} (${date})`;
+
+	var tempLiEl = document.getElementById('temp');
+	tempLiEl.textContent = `Temp: ${currentTemp}\u00B0F`;
+
+	var windLiEl = document.getElementById('wind');
+	windLiEl.textContent = `Wind: ${windSpeed}mph`;
+
+	var humidLiEl = document.getElementById('humid');
+	humidLiEl.textContent = `Humidity: ${humidity}%`;
+
+	var sunRiseLiEl = document.getElementById('sunrise');
+	sunRiseLiEl.textContent = `Sunrise: ${sunRise}AM`;
+
+	var sunSetLiEl = document.getElementById('sunset');
+	sunSetLiEl.textContent = `Sunset: ${sunSet}PM`;
+}
+
+function forecastCards(forecastData) {
+	const dates = [];
+	const temps = [];
+	const winds = [];
+	const humidity = [];
+	const icons = [];
+	const descriptions = [];
+
+	for (var i = 3; i < forecastData.cnt; i += 8) {
+		let tempDate = new Date(forecastData.list[i].dt * 1000);
+		dates.push(
+			tempDate.getMonth() +
+				1 +
+				'/' +
+				tempDate.getDate() +
+				'/' +
+				tempDate.getFullYear()
+		);
+		temps.push(Math.round(forecastData.list[i].main.temp));
+		winds.push(Math.round(forecastData.list[i].wind.speed));
+		humidity.push(forecastData.list[i].main.humidity);
+		icons.push(forecastData.list[i].weather[0].icon);
+		descriptions.push(forecastData.list[i].weather[0].description);
+	}
+	console.log('Temp: ' + temps[0]);
+	console.log(dates[0]);
+	console.log('Wind: ' + winds[0] + 'mph');
+	console.log('Humidity: ' + humidity[0] + '%');
+
+	for (var i = 0; i < dates.length; i++) {
+		let icon = document.getElementById(`img${i}`);
+		icon.setAttribute(
+			'src',
+			`http://openweathermap.org/img/wn/${icons[i]}.png`
+		);
+		icon.setAttribute('alt', descriptions[i]);
+		let title = document.getElementById(`title${i}`);
+		title.textContent = dates[i];
+		let tempLiEl = document.getElementById(`temp${i}`);
+		tempLiEl.textContent = `Temp: ${temps[i]}\u00B0F`;
+		let windLiEl = document.getElementById(`wind${i}`);
+		windLiEl.textContent = `Wind: ${winds[i]}mph`;
+		let humidLiEl = document.getElementById(`humid${i}`);
+		humidLiEl.textContent = `Humidity: ${humidity[i]}%`;
+	}
+}
